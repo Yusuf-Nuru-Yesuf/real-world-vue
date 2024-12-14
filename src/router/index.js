4dd7153ee91c260fa7419ba15cf0cde1f47e3500
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-12-14 18:13:31 +0800

    Finalize Project

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue'
+import AboutView from '@/views/AboutView.vue'
+import EventDetailsView from '@/views/EventDetailsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventListView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import('../views/AboutView.vue'), this type of routing is for performance optimization when the app is big
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView
+    }
+  ],
+})
+
+export default router
