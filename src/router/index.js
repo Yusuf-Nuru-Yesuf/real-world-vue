9d1864f9cbae4af1d04f0638b7da39b52c97dca4
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-12-15 15:10:52 +0800

    Refactor router configuration to improve event-related routes and components

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventListView from '../views/EventListView.vue'
-import AboutView from '@/views/AboutView.vue'
-import EventDetailsView from '@/views/EventDetailsView.vue'
+import EventList from '@/views/EventList.vue'
+import EventDetails from '@/views/event/Details.vue'
+import EventRegister from '@/views/event/Register.vue'
+import EventEdit from '@/views/event/Edit.vue'
+import About from '@/views/About.vue'
+import EventLayout from '@/views/event/Layout.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'event-list',
-      component: EventListView,
+      name: 'EventList',
+      component: EventList,
+      props: route => ({page: parseInt(route.query.page) || 1})
     },
     {
       path: '/about',
-      name: 'about',
-      component: AboutView
+      name: 'About',
+      component: About
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
@@ -22,10 +27,27 @@ const router = createRouter({
     },
     {
       path: '/event/:id',
-      name: 'event-details',
+      name: 'EventLayout',
       props: true,
-      component: EventDetailsView
-    }
+      component: EventLayout,
+      children: [
+        {
+          path: '',
+          name: 'EventDetails',
+          component: EventDetails
+        },
+        {
+          path: 'register',
+          name: 'EventRegister',
+          component: EventRegister
+        },
+        {
+          path: 'edit',
+          name: 'EventEdit',
+          component: EventEdit
+        }
+      ]
+    },
   ],
 })
 
