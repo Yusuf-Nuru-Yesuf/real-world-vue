e9d9b767f1a47d9ccc24f784a87c3706dbe52955
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-12-15 15:55:03 +0800

    Add registration functionality with redirect to event details

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,10 +1,18 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-defineProps(['event'])
+import { useRouter } from 'vue-router';
 
+defineProps(['event'])
+const router = useRouter()
+
+const register = () => {
+  // If resgistration api is successful, redirect to event details
+  router.push({ name: 'EventDetails' })
+}
 </script>
 
 <template>
   <p>Register for event here</p>
+  <button @click="register">Regster Me!</button>
 </template>
 
