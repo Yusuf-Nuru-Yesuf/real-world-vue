698dcf74264e55fbb794628b40bca9eb10c29dea
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-12-15 15:13:24 +0800

    Add Layout component for event details and navigation

---
+++ src/views/event/Layout.vue
@@ -0,0 +1,45 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService.js'
+
+const props = defineProps({
+  id: {
+    required: true,
+  },
+})
+
+const event = ref(null)
+
+onMounted (() => {
+  EventService.getEvent(props.id)
+  .then((response)=> {
+    event.value = response.data
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
+
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <div id="nav">
+      <router-link :to="{ name: 'EventDetails' }">
+        Details
+      </router-link>
+      |
+      <router-link :to="{ name: 'EventRegister' }">
+        Register
+      </router-link>
+      |
+      <router-link :to="{ name: 'EventEdit' }">
+        Edit
+      </router-link>
+    </div>
+    <router-view :event="event" />
+  </div>
+</template>
+
