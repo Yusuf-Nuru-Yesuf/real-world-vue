1ba0cb7d0d6199816b59e592448363f4f0af8f66
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-12-16 09:31:57 +0800

    Implement scroll behavior in router for improved navigation experience

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,13 @@ import NetworkError from '@/views/NetworkError.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
   routes: [
     {
       path: '/',
