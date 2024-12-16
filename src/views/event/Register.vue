3aef1419425e385171e117f956510a984cce71b1
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-12-16 09:30:16 +0800

    Add flash message handling in App.vue and Register.vue for user feedback

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,13 +1,17 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import { useRouter } from 'vue-router';
-import { defineProps } from 'vue'
+import { defineProps, inject } from 'vue'
 
-defineProps(['event'])
+const event = defineProps(['event'])
 const router = useRouter()
-
+const GStore = inject('GStore')
 const register = () => {
   // If resgistration api is successful, redirect to event details
+  GStore.flashMessage = 'You are successfully registered for' + event.title
+  setTimeout(() => {
+    GStore.flashMessage = ''
+  }, 3000)
   router.push({ name: 'EventDetails' })
 }
 </script>
